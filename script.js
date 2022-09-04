be0ecfd562104a3d2f55545301744f0a713430f7
Karan <karanchandekar431@gmail.com>
2022-09-04 16:13:11 +0530

    created and styled macbook seciton part 2

--- script.js
+++ script.js
@@ -93,3 +93,16 @@ const slideshow = () => {
 
 slideshow();
 // End of Slideshow
+
+// Section 3
+const section3Content = document.querySelector(".section-3-content");
+
+window.addEventListener("scroll", () => {
+  if (
+    window.pageYOffset + window.innerHeight >=
+    section3Content.offsetTop + section3Content.offsetHeight / 2
+  ) {
+    section3Content.classList.add("change");
+  }
+});
+// End of Section 3
